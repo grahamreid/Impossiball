621eb994f7c6e43e709817b27e2360245fab85c0
Graham Reid <gjreid90@gmail.com>
2014-03-11 22:29:42 -0400

    Player can now look slightly up and down. Will make into a configuration once we integrate with Occulus

--- Assets/Graham/Scripts/Generic Scripts/ControlOrientation.cs
+++ Assets/Graham/Scripts/Generic Scripts/ControlOrientation.cs
@@ -2,10 +2,23 @@
 using System.Collections;
 
 public class ControlOrientation : MonoBehaviour {
-	float floatPivotSpeed = 2f;
+
+	private float sensitivity = .25f;
+	
+	public float minimumX = -360F;
+	public float maximumX = 360F;
+	
+	public float minimumY = 0F;
+	public float maximumY = 0F;
+
+	float rotationY = 0F;
+	float rotationX = 0F;
 	// Use this for initialization
+
+	private float rotateSpeed = 2f;
 	void Start () {
-		
+		minimumY = transform.localEulerAngles.x - 40;
+		maximumY = transform.localEulerAngles.x + 40;
 	}
 	
 	void Awake () {
@@ -14,9 +27,15 @@ public class ControlOrientation : MonoBehaviour {
 	
 	void Update()
 	{
-		float floatRotationY = Input.GetAxis ("Pivot")*floatPivotSpeed;
-		if(floatRotationY < -floatPivotSpeed/4 || floatRotationY > floatPivotSpeed/4)
-			this.transform.Rotate(0,floatRotationY,0);
+		if (Mathf.Abs (Input.GetAxis ("Pivot")) > sensitivity) {
+						print ("here");
+						rotationX = transform.localEulerAngles.y + Input.GetAxis ("Pivot") * rotateSpeed;
+				}
+		if (Mathf.Abs(Input.GetAxis("Look")) > sensitivity)
+			rotationY += Input.GetAxis("Look") * rotateSpeed;
+		rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+		
+		transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
 		this.transform.position = GameObject.Find ("Sphere").transform.position;
 	}
 
